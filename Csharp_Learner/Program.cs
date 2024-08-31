d3cc2cce67e3cc50b5ee9502ea1549cf1f4b57cb
Avinash chowdary Bodduluri <143930550+avi-508@users.noreply.github.com>
2024-08-31 12:15:33 +0200

    Added program to calculate no of seats when merged given no of tables.

--- Csharp_Learner/Program.cs
+++ Csharp_Learner/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+    class program {
+    static void Main(string[] args)
+    {
+        Console.Write("Enter how many tables do u want to merge: ");
+        int no_of_tables = Convert.ToInt32(Console.ReadLine());
+        int no_of_seats = 4;
+        for(int i = 0; i < no_of_tables - 1; i++)
+        {
+            no_of_seats += 2;
+        }
+        Console.WriteLine($"Total no of seats when joining {no_of_tables} tables is {no_of_seats}");
+    }
+}
