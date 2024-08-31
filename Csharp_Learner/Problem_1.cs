f7c1e282f8a65aaedc056d0d8f891d84938d188a
Avinash chowdary Bodduluri <143930550+avi-508@users.noreply.github.com>
2024-08-31 12:28:44 +0200

    Changed program name

---
+++ Csharp_Learner/Problem_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    class Problem_1 {
+    static void Main(string[] args)
+    {
+        Console.Write("Enter how many tables do u want to merge: ");
+        int no_of_tables = Convert.ToInt32(Console.ReadLine());
+        int no_of_seats = 4;
+        for(int i = 1; i < no_of_tables ; i++)
+        {
+            no_of_seats += 2;
+        }
+        Console.WriteLine($"Total no of seats when joining {no_of_tables} tables is {no_of_seats}");
+    }
+}
